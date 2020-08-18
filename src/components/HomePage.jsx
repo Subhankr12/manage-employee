47d402ead83c86b930ac137a30d662d16e8eafda
Subhankr12 <subhankr12@gmail.com>
2020-08-18 13:15:41 +0530

    make initial setup

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import EmployeeList from "./employeeList";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <EmployeeList />
+      </div>
+    );
+  }
+}
